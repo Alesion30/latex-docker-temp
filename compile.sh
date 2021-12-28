3ad76963f4807e5fc452d0c013336993c8125ad9
Yuta Ohira <alesion90@gmail.com>
2021-12-28 13:10:21 +0900

    :sparkles: bibを追加

--- compile.sh
+++ compile.sh
@@ -6,12 +6,16 @@ TARGET=$1 # コンパイルの対象
 # 実行コマンド
 #----------------------------------------------------------------
 TEX=platex
-# BIB=pbibtex
+BIB=pbibtex
 PDF=dvipdfmx
 
 docker run --rm -v "$PWD":/workdir paperist/alpine-texlive-ja $TEX "$TARGET".tex
-docker run --rm -v "$PWD":/workdir paperist/alpine-texlive-ja $PDF "$TARGET".tex
-rm -f "$TARGET".{aux,dvi,log,out,toc}
+docker run --rm -v "$PWD":/workdir paperist/alpine-texlive-ja $BIB "$TARGET".aux
+docker run --rm -v "$PWD":/workdir paperist/alpine-texlive-ja $PDF "$TARGET".dvi
+
+rm -f *.{aux,dvi,log,out,toc,bbl,blg}
+
+# PDFファイルをbuildフォルダに移動
 if [ -e "$TARGET".pdf ]; then
     if [ ! -e build/ ]; then
         mkdir build
